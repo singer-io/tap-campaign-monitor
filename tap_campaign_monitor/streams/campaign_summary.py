e554d97148f3076adac4073dd51a36b976805b1d
Connor McArthur <cmcarthur@rjmetrics.com>
2018-06-29 10:58:24 -0400

    fix sync data for single-item child endpoints

--- tap_campaign_monitor/streams/campaign_summary.py
+++ tap_campaign_monitor/streams/campaign_summary.py
@@ -20,3 +20,24 @@ class CampaignSummaryStream(ChildStream):
 
     def get_stream_data(self, result):
         return result
+
+    def sync_data(self, parent=None):
+        if parent is None:
+            raise RuntimeError('Cannot sync a subobject of null!')
+
+        table = self.TABLE
+        url = (
+            'https://api.createsend.com/api/v3.2{api_path}'.format(
+                api_path=self.get_api_path_for_child(parent)))
+
+        result = self.client.make_request(url, self.API_METHOD)
+
+        data = self.get_stream_data(result)
+
+        with singer.metrics.record_counter(endpoint=table) as counter:
+            singer.write_records(
+                table,
+                [self.filter_keys(
+                    self.incorporate_parent_id(data, parent))])
+
+            counter.increment()
