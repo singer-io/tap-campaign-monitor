f56afd1cc9f12d654f81ab041d015cf644745597
Connor McArthur <connor@fishtownanalytics.com>
2018-08-08 16:39:41 -0400

    backoff

--- tap_campaign_monitor/client.py
+++ tap_campaign_monitor/client.py
@@ -2,6 +2,7 @@ import requests
 import requests.auth
 import singer
 import singer.metrics
+import time
 
 LOGGER = singer.get_logger()  # noqa
 
@@ -16,7 +17,8 @@ class CampaignMonitorClient:
             self.config.get('api_key'),
             'x')
 
-    def make_request(self, url, method, params=None, body=None):
+    def make_request(self, url, method, base_backoff=30,
+                     params=None, body=None):
         auth = self.get_authorization()
 
         LOGGER.info("Making {} request to {}".format(method, url))
@@ -31,7 +33,19 @@ class CampaignMonitorClient:
             params=params,
             json=body)
 
-        if response.status_code != 200:
+        if response.status_code in [429, 504]:
+            if base_backoff > 120:
+                raise RuntimeError('Backed off too many times, exiting!')
+
+            LOGGER.warn('Sleeping for {} seconds and trying again'
+                        .format(base_backoff))
+
+            time.sleep(base_backoff)
+
+            return self.make_request(
+                url, method, base_backoff * 2, params, body)
+
+        elif response.status_code != 200:
             raise RuntimeError(response.text)
 
         return response.json()
