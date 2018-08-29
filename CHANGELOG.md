6f96cbe77f9fe5a13db6b1e3a8e128b81ec2a755
Connor McArthur <connor@fishtownanalytics.com>
2018-08-29 13:38:27 -0400

    0.1.1: include all relevant files via setup.py

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1
+
+- Fix setup.py to include all the relevant files
+
 ## 0.1.0
 
 - Use metadata in catalog, instead of old style `inclusion` and `selected-by-default` fields ([#5](https://github.com/fishtown-analytics/tap-campaign-monitor/pull/5))
--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-import os.path
 
-setup(name='tap-campaign-monitor',
-      version='0.1.0',
-      description='Singer.io tap for extracting data from the Campaign Monitor API',
-      author='Fishtown Analytics',
-      url='http://fishtownanalytics.com',
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['tap_campaign_monitor'],
-      install_requires=[
-          'tap-framework==0.0.4'
-      ],
-      entry_points='''
-          [console_scripts]
-          tap-campaign-monitor=tap_campaign_monitor:main
-      ''',
-      packages=['tap_campaign_monitor'])
+setup(
+    name='tap-campaign-monitor',
+    version='0.1.1',
+    description=(
+        'Singer.io tap for extracting data from the '
+        'Campaign Monitor API'
+    ),
+    author='Fishtown Analytics',
+    url='http://fishtownanalytics.com',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    install_requires=[
+        'tap-framework==0.0.4'
+    ],
+    entry_points='''
+        [console_scripts]
+        tap-campaign-monitor=tap_campaign_monitor:main
+    ''',
+    packages=find_packages(),
+    package_data={
+        'tap_campaign_monitor': [
+            'schemas/*.json'
+        ]
+    }
+)
