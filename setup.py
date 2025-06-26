469896ba1950b051259ed6fa71f82ba1d7f962e6
Tushar Mittal <tushar.mittal@qlik.com>
2025-06-26 12:24:45 +0530

    requests library updated

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires=[
         'singer-python==6.1.1',
         'backoff==2.2.1',
-        'requests==2.32.3',
+        'requests==2.32.4',
     ],
     extras_require = {
       "dev": [
