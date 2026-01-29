c2c836a6e473f91cd600864a40151af9b8a82a21
Prijen Khokhani <88327452+prijendev@users.noreply.github.com>
2026-01-29 15:43:23 +0530

    Bump tap version (#13)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tap-campaign-monitor',
-    version='1.0.0',
+    version='1.1.0',
     description=(
         'Singer.io tap for extracting data from the '
         'Campaign Monitor API'
