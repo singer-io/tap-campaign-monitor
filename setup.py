76087e654d5220d8e18bcdb30d41966d0db5d932
akkumar-qlik <akarsh.kumar@qlik.com>
2026-03-26 07:17:34 +0530

    Minor setup changes

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     url='https://www.stitchdata.com',
     classifiers=['Programming Language :: Python :: 3 :: Only'],
     install_requires=[
-        'tap-framework==0.0.5',
         'singer-python==6.8.0',
     ],
     entry_points='''
