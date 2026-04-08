df6c7076f30af40c0a6e649e7ff5581ceb71d560
akkumar-qlik <akarsh.kumar@qlik.com>
2026-04-09 02:46:09 +0530

    Added copilot review comment changes

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     author='Stitch Data, Fishtown Analytics',
     url='https://www.stitchdata.com',
     classifiers=['Programming Language :: Python :: 3 :: Only'],
+    python_requires='>=3.12',
     install_requires=[
         'singer-python==6.8.0',
         'requests==2.32.5',
