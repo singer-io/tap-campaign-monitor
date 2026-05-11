0e1a3a3b43dced46c29eaf8149480ceeef3cc0cf
akkumar-qlik <akarsh.kumar@qlik.com>
2026-05-11 10:37:23 +0530

    Updated library version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     classifiers=['Programming Language :: Python :: 3 :: Only'],
     install_requires=[
         'singer-python==6.8.0',
-        'requests==2.32.5',
+        'requests==2.33.1',
         'backoff==2.2.1',
     ],
     extras_require={
